51eb3f916b65e96e64e7b1aa05772a1325598b5d
John Reiner <johnreiner1990@gmail.com>
2020-01-15 14:48:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(array)
   array.any? { |i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  result = array.find { |cheese| cheese.include?(cheese_types[0] || cheese_types[1] || cheese_types[2])}
 end
