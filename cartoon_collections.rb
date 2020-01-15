355c36e257d58a3d970ab690287819bdfcc096cd
John Reiner <johnreiner1990@gmail.com>
2020-01-15 14:21:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  numb = 0
+  array.each do |i|
+    numb += 1
+    puts "#{numb}. #{i}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |i|
+    "#{i.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  array.any? { |i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
